09cee75b5b37ab45a0f785cf16a26182629f31f0
Vltemadev <cfpro.tema@mail.ru>
2022-12-06 20:59:06 +0600

    second commit

--- setings.py
+++
@@ -1,3 +0,0 @@
-token = '5809516337:AAG3N-41ALJTeZ8FmkCalMBgE-O_9H9S15Q'
-
-bot_url = 't.me/intcSimpleBot'
--- settings.py
+++ settings.py
@@ -1,4 +1,2 @@
-
-
 token = '5809516337:AAG3N-41ALJTeZ8FmkCalMBgE-O_9H9S15Q'
 bot_url = 't.me/intcSimpleBot'
